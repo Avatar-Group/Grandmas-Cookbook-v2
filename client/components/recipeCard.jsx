fa1dce31657e21e39fb1d1b1e10dd0ae70f353df
Sam Blackburn <sam.r.blackburn@icloud.com>
2023-04-22 12:44:54 -0500

    preliminary style changes

--- client/components/recipeCard.jsx
+++ client/components/recipeCard.jsx
@@ -5,26 +5,13 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import { useSelector, useDispatch } from 'react-redux';
-import MoreButton from "./recipeCardButtons/MoreButton.jsx";
+import { useDispatch } from 'react-redux';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MoreButton from './recipeCardButtons/MoreButton.jsx';
 import { deleteCard } from '../slices/cardSlice';
 
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     "& .MuiPaper-root": {
-//       background: 'black'
-//     }
-//   }
-// }));
-
-
 function RecipeCard({ recipe, children, type, addHandler }) {
-  // need to loop through the the fetch data
-  // console.log('type', type)
-  // const [saveEdit, setSaveEdit] = useToggle();
-
   const dispatch = useDispatch();
 
   const [deleteButton, setDeleteButton] = React.useState(true);
@@ -33,39 +20,40 @@ function RecipeCard({ recipe, children, type, addHandler }) {
     fetch(`/recipe/delete/${recipe.id}`, {
       method: 'DELETE',
     })
-    .then((res) => {
-      if (res.ok) return dispatch(deleteCard(recipe));
-      throw new Error(res.status);
-    })
-    .catch((err) => console.log(`Error code: ${err}`));
+      .then((res) => {
+        if (res.ok) return dispatch(deleteCard(recipe));
+        throw new Error(res.status);
+      })
+      .catch((err) => console.log(`Error code: ${err}`));
   };
 
   if (deleteButton)
     return (
-      <Card sx={{ maxWidth: 600,
-      }}
-      style={{ border: "none", background:'#DDBEA9' }}
-       >
+      <Card
+        sx={{ maxWidth: 600 }}
+        style={{ border: 'none', background: '#DDBEA9' }}
+      >
         <CardMedia
           component="img"
           alt="recipe image"
-          sx={{width: '258px', height: '256px',  alignItems:'flex-end'}}
+          sx={{ width: '258px', height: '256px', alignItems: 'flex-end' }}
           // height="140"
           image={recipe.imagePath}
         />
-        <CardContent >
-          <Typography
-            gutterBottom
-            variant="h6"
-            component="div"
-          >
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
             {recipe.title}
           </Typography>
         </CardContent>
-        <CardActions> 
-          {type === 'addForm' ? <Button color="success" onClick={addHandler(recipe)}>Add</Button> : null}
-          <MoreButton recipe={recipe}/>
-          <Button color="error"  size="small" onClick={setDeleteButtonLogic}>
+        <CardActions>
+          {type === 'addForm' ? (
+            <Button color="success" onClick={addHandler(recipe)}>
+              Add
+            </Button>
+          ) : null}
+          <MoreButton recipe={recipe} />
+          {/* Add logic to only show delete for user's own recipes */}
+          <Button color="error" size="small" onClick={setDeleteButtonLogic}>
             Delete
           </Button>
         </CardActions>
