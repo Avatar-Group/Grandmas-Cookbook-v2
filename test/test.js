a063eac256d24f1d985a9cfbb986cde4ccbb8bad
KP824 <112226540+KP824@users.noreply.github.com>
2023-07-05 10:33:09 -0700

    added mocha

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const assert = require('assert');
+const { describe, it } = require('mocha');
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
